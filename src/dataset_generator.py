7b8654f4a2fa4012a9ceff4b1ca15203f4706791
KristoffLiu <zhikangliu@hotmail.com>
2021-09-08 21:53:18 +0800

    update result saving directory

--- src/dataset_generator.py
+++ src/dataset_generator.py
@@ -11,11 +11,11 @@ from env.environment import Environment
 from tasks.clear_one_obstacle import ClearObstaclesTask
 
 flags.DEFINE_string('task', 'clear-one-obstacles-task', '')
-flags.DEFINE_string('data_dir', './datasets', '')
-flags.DEFINE_bool('disp', True, '')
+flags.DEFINE_string('data_dir', '../results/datasets', '')
+flags.DEFINE_bool('disp', False, '')
 flags.DEFINE_bool('shared_memory', False, '')
-flags.DEFINE_string('mode', 'test', '')
-flags.DEFINE_integer('n', 50, '')
+flags.DEFINE_string('mode', 'train', '')
+flags.DEFINE_integer('n', 300, '')
 flags.DEFINE_integer('steps_per_seg', 3, '')
 
 FLAGS = flags.FLAGS
